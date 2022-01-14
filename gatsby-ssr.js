8b21a24bf780af52e501c03f26be4959bb1d0c83
Mandy <mandy@wearecapicua.com>
2022-01-14 19:09:48 -0300

    test xstyled theme

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,19 +1,9 @@
 import React from 'react'
-// custom typefaces
-import "typeface-montserrat"
-import "typeface-merriweather"
 import {
   defaultTheme,
   ThemeProvider,
   Preflight,
 } from '@xstyled/styled-components'
-// normalize CSS across browsers
-//import "./src/normalize.css"
-// custom CSS styles
-//import "./src/style.css"
-
-// Highlighting for code blocks
-//import "prismjs/themes/prism.css"
 
 const theme = {
   ...defaultTheme,
